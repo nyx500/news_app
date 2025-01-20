4e645bf5df86a6296883cdf6555adc974301132e
Ophelia Lavey <ophelia@mac.home>
2025-01-20 19:05:42 +0100

    .

--- lime_functions.py
+++ lime_functions.py
@@ -60,7 +60,7 @@ class BasicFeatureExtractor:
     """
     
     def __init__(self):
-        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp = spacy.load("en_core_web_lg")
 
     def extractExclamationPointFreqs(self, text):
         """
